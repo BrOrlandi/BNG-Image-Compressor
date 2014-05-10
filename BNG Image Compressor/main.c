d1a5332c74dedfedb21a4821e886ec5596a243a0
Bruno Orlandi <brorlandi@gmail.com>
2014-05-10 17:57:26 -0300

    Novo huffman na Main.

--- BNG Image Compressor/main.c
+++ BNG Image Compressor/main.c
@@ -2,55 +2,82 @@
 #include <stdlib.h>
 
 #include "bitmap.h"
-#include "bitwriter.h"
-#include "huffman.h"
-#include "fileio.h"
+#include "huffman/huffman.h"
 #include <string.h>
 
 
 int main(int argc, char* argv[]) {
 
-    if(argc < 2) {
-        printf("usage: \t\tbng <file>");
-        return 1;
+    if(argc < 3){ // the file must be in the arguments
+        printf("Usage: <input file> [optional -d] <output file>\n\t -d\tThe input file will be decompressed to output file.");
+        return 0;
     }
 
-    char *fileName = argv[1]; // name of the file to be compressed or decompressed
+    char *inputFile = argv[1];
+    char *outputFile;
 
-    if(endsWith(fileName,".bng")){ // check if the file is an '.bng' file, if it is, decompress
+    char decompress = 0;
+    if(!strcmp(argv[2],"-d")){
+        decompress = 1;
+        if(argc < 4){
+            printf("Missing <output file>\n");
+            return 0;
+        }
+        outputFile = argv[3];
+    }
+    else{
+        outputFile = argv[2];
+    }
+
+    Huffman h;
+
+    Huffman_init(&h);
+
+    if(decompress){
+
+        printf("Decompressing: %s\n", inputFile);
 
         unsigned int size; // size of the file to be calculated
         unsigned char *data; // new data after decompress
 
-        huffmanFileRead(fileName,&data,&size);
+        Huffman_file_decompress(&h,inputFile);
+        data = h.uc_data[0];
+        size = h.uc_sizes[0];
 
         // here can make others manipulations to the data
-
-        printf("Saving the uncompressed file.\n");
-        int len = strlen(fileName);
-        fileName[len-4] = '\0';
-        fileWrite(fileName,data,size);
-        printf("File decompressed: %s\n",fileName);
+        fileWrite(outputFile,data,size);
+        printf("Image decompressed to: %s\n",outputFile);
     }
     else{ // otherwise, the file will be compres8sed in a bng file
 
-        FILE *image;
-        BMPData bitmap;
+        //FILE *image;
+        //BMPData bitmap;
 
-        unsigned int size;
-        image = fopen(fileName, "rb");
-        unsigned char *data = fileRead(fileName,&size);
-        if(image == NULL){
-            printf("File not found: %s\n",fileName);
-            exit(1);
-        }
-        BMPData_init(&bitmap, image);
-        BMPData_print(&bitmap);
+        //unsigned int size;
+        //image = fopen(fileName, "rb");
+        //unsigned char *data = fileRead(fileName,&size);
+        //if(image == NULL){
+        //    printf("File not found: %s\n",fileName);
+        //    exit(1);
+        //}
+       // BMPData_init(&bitmap, image);
+       // BMPData_print(&bitmap);
 
 
-        huffmanFileWrite(fileName,data,size);
+        unsigned int size; // size of the file to be calculated
+        unsigned char *data; // the data read from the file
 
-        BMPData_destroy(&bitmap);
+        data = fileRead(inputFile,&size); // read the file and stores in data
+
+        printf("Compressing: %s\n", inputFile);
+        Huffman_add_data_block(&h,data,size);
+
+        Huffman_apply(&h);
+
+        Huffman_compress_data_to_file(&h,outputFile);
+        printf("Image compressed to: %s\n",outputFile);
+
+        //BMPData_destroy(&bitmap);
 
     }
     return 0;
---
+++ BNG Image Compressor/run.bat
@@ -0,0 +1,6 @@
+".\bin\Debug\BNG Image Compressor.exe" "red.bmp" "red.bmp.hff"
+".\bin\Debug\BNG Image Compressor.exe" "red.bmp.hff" -d "decompressed red.bmp"
+
+."\bin\Debug\BNG Image Compressor.exe" "white.bmp" "white.bmp.hff"
+".\bin\Debug\BNG Image Compressor.exe" "white.bmp.hff" -d "decompressed white.bmp"
+pause
