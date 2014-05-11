5a9f0fa982163739901326561297176209f0f257
takizawa <nihey.takizawa@gmail.com>
2014-05-11 19:00:58 -0300

    Encoding and decoding with some inconsistencies.

---
+++ BNG Image Compressor/run_len.c
@@ -0,0 +1,112 @@
+#include "run_len.h"
+
+void sanitize(unsigned char *vector, int data_size) {
+    int i;
+    for(i = 0; i < data_size; i++) {
+        if(vector[i] == RUN_LEN_FLAG) {
+            vector[i]++;
+        }
+    }
+}
+
+unsigned char *encode(unsigned char *vector, int width, int height, int *size) {
+    int data_size = width * height * 3;
+    unsigned char *rle = (unsigned char*) calloc(data_size+2+8, sizeof(unsigned char));
+
+    sanitize(vector, data_size);
+
+    //CODIFICAÇÃO POR CARREIRA
+    int count = 1; //CONTA A QUANTIDADE DE REPETIÇÕES
+    int writer = 0; //CONTA A POSIÇÃO DE ESCRITA NO VETOR CODIFICADO
+    int i, j;
+
+    // These are reserved fields, they store the matrix original width
+    // and height
+    rle[writer++] = (width & 0xFF);
+    rle[writer++] = (width >> 8) & 0xFF;;
+    rle[writer++] = (width >> 16) & 0xFF;
+    rle[writer++] = (width >> 24) & 0xFF;
+
+    rle[writer++] = (height & 0xFF);
+    rle[writer++] = (height >> 8) & 0xFF;
+    rle[writer++] = (height >> 16) & 0xFF;
+    rle[writer++] = (height >> 24) & 0xFF;
+
+    unsigned char last_char = vector[0];
+    for(i = 1; i<data_size; i++)
+    {
+        if(last_char == vector[i]) { // Keep counting the appearances
+            count++;
+            if(count >= 255) {
+                goto FLUSH;
+            }
+        }
+        else {
+            if(count >= 3) { // If count is greater than 3, value is run-length coded
+                FLUSH:
+                rle[writer++] = RUN_LEN_FLAG;
+                rle[writer++] = count;
+                rle[writer++] = last_char;
+                count = 1;
+            }
+            else {
+                for(j = 0; j < count; j++) {
+                    rle[writer++] = last_char;
+                }
+                count = 1;
+            }
+        }
+        last_char = vector[i];
+    }
+    if(count >= 3) { // If count is greater than 3, value is run-length coded
+        rle[writer++] = RUN_LEN_FLAG;
+        rle[writer++] = count;
+        rle[writer++] = last_char;
+        count = 1;
+    }
+    else {
+        for(j = 0; j < count; j++) {
+            rle[writer++] = last_char;
+        }
+        count = 1;
+    }
+    // Duas flags são o valor sentinela que representa o fim da decodificação.
+    rle[writer++] = RUN_LEN_FLAG;
+    rle[writer++] = RUN_LEN_FLAG;
+//    for(j = 0; j < writer; j++) {
+//        printf("%d ",rle[j]);
+//    }
+//    printf("\n");
+//    for(j = 0; j < data_size; j++) {
+//        printf("%d ",vector[j]);
+//    }
+    printf("\n");
+
+    *size = writer;
+    return rle;
+}
+
+unsigned char *decode(unsigned char *rle, int data_size) {
+
+    int i = 8, j, count = 0;
+
+    unsigned char *rle_dec = (unsigned char*) calloc(data_size, sizeof(unsigned char));
+
+    while(1) {
+        //printf("%d -> %d << %d\n", i, data_size, count);
+        if(rle[i++] == RUN_LEN_FLAG) {
+            if(rle[i++] == RUN_LEN_FLAG) {
+                break;
+            }
+            for(j = 0; j < rle[i-1]; j++) {
+                rle_dec[count++] = rle[i];
+            }
+            i++;
+        }
+        else {
+            rle_dec[count++] = rle[i-1];
+        }
+    }
+
+    return rle_dec;
+}
---
+++ BNG Image Compressor/zigzag_vector.h
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "bitmap.h"
+
+#ifndef ZIGZAG_H
+
+#define ZIGZAG_H
+
+unsigned char *vectorize(BMPData *bmp);
+
+unsigned char *unvectorize(unsigned char *zigzag, int size);
+
+#endif // ZIGZAG_H
